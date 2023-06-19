39907866c10dda7d02192382cad542fad16fcb0d
CassianeAlmeida21 <cassianealmeida66@gmail.com>
2023-06-19 06:23:34 -0300

    primeiro commit

---
+++ Terreno - PO2/Program.cs
@@ -0,0 +1,7 @@
+﻿Terreno t = new Terreno(6, 6);
+
+Console.WriteLine($"A area do terreno é: {t.AreaTerreno()}");
+
+Console.WriteLine($"\nO valor estimado do terreno é: {t.ValorEstimado(100)}");
+
+
